1a4651017f57e607c5a5e4687c9e8fd1bc73f2e3
LifengWang <wanglifeng0123@163.com>
2015-08-04 11:09:04 +0800

    clean the useless files

--- engines/impala/queries/t.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#if [ -a `grep hiveconf -rl ./q23/*` ]
-#then
-#echo "================"
-#fi
-
-grep hionf -rl ./q23/*
-echo $?
--- engines/impala/queries_default/t.sh
+++
@@ -1,4 +0,0 @@
-sed s/'$QUERY_SQL_DIR/$QUERY_NAME.sql'/'a'/ q01/run.sh 
-
-var='$QUERY_SQL_DIR/$QUERY_NAME.sql'
-sed -i s#"$var""$VALUE"/g `grep hiveconf -rl $QUERY_SQL_DIR/*.sql`
